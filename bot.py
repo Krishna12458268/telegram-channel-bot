189dd3e51a57603042454fcd68699125eb163226
Krishna12458268 <krishna@launchigo.in>
2025-05-22 11:36:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "YOUR_BOT_TOKEN"  # ← Yahan apna real token daalo
+TOKEN = "8090320571:AAGPMBXpbV7JgIqjgr6iQ_eAP30kXZGnxNE"  # ← Yahan apna real token daalo
 CHANNEL_LINK = "https://t.me/yourchannelusername"  # ← Apna channel link daalo
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
